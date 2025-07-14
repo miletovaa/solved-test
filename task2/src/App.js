c511cf67ef7e94383904ab56293c94ff647f2d17
anna miletova <annamiletova@gmail.com>
2025-07-14 20:17:12 +0300

    2.3. Added comments

--- task2/src/App.js
+++ task2/src/App.js
@@ -14,6 +14,20 @@ function App() {
     }
   }
 
+  const loadComments = async (postId) => {
+    try {
+      const comments = await fetchData(`posts/${postId}/comments`)
+      console.log(comments)
+      setPosts(prevPosts => 
+        prevPosts.map(post => 
+          post.id === postId ? { ...post, comments } : post
+        )
+      )
+    } catch (error) {
+      console.error('Failed to load comments:', error)
+    }
+  }
+
   return (
     <>
       <button onClick={loadPosts}>
@@ -22,7 +36,14 @@ function App() {
       {posts && (
         <ul>
           {posts.map(post => (
-            <li key={post.id}>{post.title}</li>
+            <>
+              <li key={post.id} onClick={() => loadComments(post.id)}>{post.title}</li>
+              {post?.comments && post.comments.map(comment => (
+                <ul key={comment.id}>
+                  <li>{comment.body}</li>
+                </ul>
+              ))}
+            </>
           ))}
         </ul>
       )}
